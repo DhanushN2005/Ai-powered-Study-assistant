238f2ae261cf90671c45915ed9da14479d0e4fb6
Dhanush N <717823i108@kce.ac.in>
2026-02-02 18:31:51 +0530

    Fix email timeout: Switch to Gmail service in nodemailer

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -2,9 +2,7 @@ const nodemailer = require('nodemailer');
 
 const sendEmail = async (options) => {
     const transporter = nodemailer.createTransport({
-        host: process.env.EMAIL_HOST,
-        port: process.env.EMAIL_PORT,
-        secure: false,
+        service: 'gmail',
         auth: {
             user: process.env.EMAIL_USER,
             pass: process.env.EMAIL_PASSWORD,
